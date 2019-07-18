f183d00df0080bcd49cd657c03a9e5475e5584e5
Wendy Sanarwanto <wendy.sanarwanto@mitrais.com>
2019-07-18 14:56:24 +0800

    Initial push

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import {Header} from './common/Header';
+
+const App = props => {
+  return (
+    <View>
+      <Header title="Manager" />
+    </View>
+  );
+};
+
+export default App;
---
+++ src/components/common/Header.js
@@ -0,0 +1,36 @@
+// Import libraries for making a component 
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// Make a component
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+  const { title } = props;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20,
+  },
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 1.5,
+    position: 'relative'
+  }
+};
+
+// Make the component available to other parts of the App
+export { Header };
