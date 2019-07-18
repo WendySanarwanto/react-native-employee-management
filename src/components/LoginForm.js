a2022076d323110b3ef2d8a8c3c3585ec453e6dc
Wendy Sanarwanto <wendy.sanarwanto@mitrais.com>
2019-07-18 18:20:15 +0800

    Handle email input's onchange event

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import { Card, CardSection, Input, Button } from './common';
+import { emailChanged } from '../actions';
 
 class LoginForm extends Component {
+  // state = { email: null, password: null };
+  onEmailChange(text) {
+    // const email = e.target.value;
+    // debugger;
+    // console.log(`[DEBUG]<onEmailChange> text: ${text}`);
+    emailChanged(text);
+  }
+
   render() {
     return (
       <Card>
         <CardSection>
-          <Input label="Email" placeholder="e.g. john.smith@gmail.com" />
+          <Input
+            label="Email"
+            placeholder="e.g. john.smith@gmail.com"
+            onChangeText={this.onEmailChange.bind(this)}
+          />
         </CardSection>
         <CardSection>
           <Input secureTextEntry label="Password" placeHolder="password" />
@@ -20,4 +34,9 @@ class LoginForm extends Component {
   }
 }
 
-export default LoginForm;
+export default connect(
+  null,
+  {
+    emailChanged,
+  }
+)(LoginForm);
