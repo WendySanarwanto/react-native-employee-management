cc9b933ec276fea79c07e5bba158ea55fc0e3bb6
Wendy Sanarwanto <wendy.sanarwanto@mitrais.com>
2019-07-18 16:47:55 +0800

    Integrate firebase app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View } from 'react-native';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import firebase from 'firebase';
 
 import { Header } from './components/common/Header';
 import reducers from './reducers';
@@ -10,6 +11,21 @@ import reducers from './reducers';
 const appStore = createStore(reducers, applyMiddleware(thunk));
 
 const App = props => {
+  // componentDidMount
+  useEffect(() => {
+    // console.log(`[DEBUG]<App> 'componentDidMount' event is fired`);
+    var firebaseConfig = {
+      apiKey: 'AIzaSyArkBzTS3vZbftAuDQTUoDO4c_re5UVFXk',
+      authDomain: 'rn-employee-management.firebaseapp.com',
+      databaseURL: 'https://rn-employee-management.firebaseio.com',
+      projectId: 'rn-employee-management',
+      storageBucket: '',
+      messagingSenderId: '460944330860',
+      appId: '1:460944330860:web:265fd4967f6a42b3',
+    };
+    // Initialize Firebase
+    this.fbaseApp = firebase.initializeApp(firebaseConfig);
+  });
   return (
     <Provider store={appStore}>
       <View style={styles.mainViewStyle}>
