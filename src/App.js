837caaa425a5c1f4b1d0adfd67731dfabc6bcdb4
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-07-21 01:44:51 +0800

    Moved firebase config to local .env file

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import firebase from 'firebase';
 
+import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_DB_URL, 
+  FIREBASE_PROJECT_ID, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID } from 'react-native-dotenv';
 import reducers from './reducers';
 import Router from './Router';
 
@@ -15,13 +17,13 @@ const App = _ => {
   useEffect(() => {
     // console.log(`[DEBUG]<App> 'componentDidMount' event is fired`);
     var firebaseConfig = {
-      apiKey: 'AIzaSyArkBzTS3vZbftAuDQTUoDO4c_re5UVFXk',
-      authDomain: 'rn-employee-management.firebaseapp.com',
-      databaseURL: 'https://rn-employee-management.firebaseio.com',
-      projectId: 'rn-employee-management',
+      apiKey: FIREBASE_API_KEY,
+      authDomain: FIREBASE_AUTH_DOMAIN,
+      databaseURL: FIREBASE_DB_URL,
+      projectId: FIREBASE_PROJECT_ID,
       storageBucket: '',
-      messagingSenderId: '460944330860',
-      appId: '1:460944330860:web:265fd4967f6a42b3',
+      messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+      appId: FIREBASE_APP_ID,
     };
     // Initialize Firebase. This should be happened at once only
     this.fbaseApp = firebase.initializeApp(firebaseConfig);
