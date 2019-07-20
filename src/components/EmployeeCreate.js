0c956ea811868fdafbe5a6d521b03db10d42efd8
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-07-21 03:35:26 +0800

    Added name input, phone input and save button

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,47 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Input, Button, Card, CardSection } from '../components/common';
+import { connect } from 'react-redux';
+
+import { employeeUpdate } from '../actions/EmployeeActions';
 
 class EmployeeCreate extends Component {
   render(){
     return (
-      <View><Text>Employee Create !</Text></View>
+      <Card>
+        <CardSection>
+          <Input 
+            label="Name" 
+            placeHolder="Jane" 
+            value={ this.props.name }
+            onChangeText={ name => this.props.employeeUpdate({ prop: 'name', value: name }) } />
+        </CardSection>
+
+        <CardSection>
+          <Input 
+            label="Phone" 
+            placeHolder="555-555-5555" 
+            value={ this.props.phone }
+            onChangeText={ phone => this.props.employeeUpdate({ prop: 'phone', value: phone }) }/>
+        </CardSection>
+
+        <CardSection>
+
+        </CardSection>
+
+        <CardSection>
+          <Button label="Save"  />
+        </CardSection>
+      </Card>
     );
   }
 };
 
-export default EmployeeCreate;
+function mapStateToProps({ name, phone, shift }) {
+  return {
+    name,
+    phone,
+    shift,
+  };
+}
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
