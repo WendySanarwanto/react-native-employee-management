95bcb5431a2fbd8062f5edb13783d239a1515086
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-07-20 19:44:34 +0800

    Added employee create Scene

--- src/Router.js
+++ src/Router.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Actions, Scene, Router } from 'react-native-router-flux';
 
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
   return (
     <Router>
       <Scene key="root" hideNavBar>
+
         <Scene key="auth">
-          <Scene key="login" component={ LoginForm } title="Please Login" initial />
+          <Scene key="login" component={ LoginForm } title="Please Login" />
         </Scene>
+
         <Scene key="main">
           <Scene
             rightTitle="Add"
-            onRight={() => console.log(`[DEBUG] Add Employee button is clicked.`)}
+            onRight={() => Actions.employeeCreate()}
             key="employeeList" 
             component={ EmployeeList } 
-            title="Employees" />
+            title="Employees"
+            initial />
+          <Scene key="employeeCreate" component={ EmployeeCreate } title="Create Employee" />
         </Scene>
+
       </Scene>
     </Router>
   );
---
+++ src/components/EmployeeCreate.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class EmployeeCreate extends Component {
+  render(){
+    return (
+      <View><Text>Employee Create !</Text></View>
+    );
+  }
+};
+
+export default EmployeeCreate;
